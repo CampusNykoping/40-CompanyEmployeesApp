9cacc5779173a22994e356545c3edaabe18aff1b
Claes Engelin <claes.engelin@zocom.se>
2022-05-03 13:09:34 +0200

    Efter Kapitel 3
    Gjort klart Service Manager

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -3,6 +3,8 @@ using Contracts;
 
 using LoggerService;
 
+using Microsoft.EntityFrameworkCore;
+
 using Repository;
 
 using Service;
@@ -33,4 +35,9 @@ public static class ServiceExtensions
 
     public static void ConfigureServiceManager(this IServiceCollection services) =>
         services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+}
 }

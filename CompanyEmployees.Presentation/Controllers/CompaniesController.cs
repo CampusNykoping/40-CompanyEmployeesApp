00a9815587c4e9221c00ff30f589fbcfcb930b78
Claes Engelin <claes.engelin@zocom.se>
2022-05-10 17:47:18 +0200

    Efter kapitel 9

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,6 @@
 ﻿
+using CompanyEmployees.Presentation.ModelBinders;
+
 using Microsoft.AspNetCore.Mvc;
 
 using Service.Contracts;
@@ -32,6 +34,14 @@ public class CompaniesController : ControllerBase
         return Ok(company);
     }
 
+    [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+    public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
+    {
+        var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+
+        return Ok(companies);
+    }
+
     [HttpPost]
     public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
     {
@@ -44,5 +54,15 @@ public class CompaniesController : ControllerBase
         return CreatedAtRoute("CompanyById", 
             new { id = createdCompany.Id }, 
             createdCompany);
+    }
+
+    [HttpPost("collection")]
+    public IActionResult CreateCompanyCollection(
+        [FromBody]IEnumerable<CompanyForCreationDto> companyCollection)
+    {
+        var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
+
+        return CreatedAtRoute("CompanyCollection",
+            new {result.ids}, result.companies);
     }
 }
--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -8,4 +8,7 @@ public interface ICompanyService
     IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     CompanyDto GetCompany(Guid comanyId, bool trackChanges);
     CompanyDto CreateCompany(CompanyForCreationDto company);
+    IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+    (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection
+        (IEnumerable<CompanyForCreationDto> companyCollection);
 }

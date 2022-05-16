f3388cf2231939b67345a179ab8f2a399a41d5ae
Claes Engelin <claes.engelin@zocom.se>
2022-05-16 11:11:24 +0200

    Efter kapitel 11
    Lagt till Update av Companies, även med samtidig Create av Employees

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -11,4 +11,9 @@ public interface IEmployeeService
         bool trackChanges);
 
     void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+
+    void UpdateEmployeeForCompany(Guid companyId, Guid id,
+        EmployeeForUpdateDto employeeForUpdate,
+        bool compTrackChanges,
+        bool empTrackChanges);
 }
